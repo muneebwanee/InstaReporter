2ad0bc87ab0b19f6daafb6a72d2c6890716c8122
muneebwanee <muneebwanee@gmail.com>
2020-12-08 19:30:21 +0530

    Add files via upload

---
+++ libs/logo.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+#!/usr/bin/env python3
+from colorama import Fore, Back, Style
+from random import choice
+
+logo = """
+╭━━╮╱╱╱╱╱╭╮╱╱╱╭━━━╮╱╱╱╱╱╱╱╱╱╱╭╮
+╰┫┣╯╱╱╱╱╭╯╰╮╱╱┃╭━╮┃╱╱╱╱╱╱╱╱╱╭╯╰╮
+╱┃┃╭━╮╭━┻╮╭╋━━┫╰━╯┣━━┳━━┳━━┳┻╮╭╋━━┳━╮
+╱┃┃┃╭╮┫━━┫┃┃╭╮┃╭╮╭┫┃━┫╭╮┃╭╮┃╭┫┃┃┃━┫╭╯
+╭┫┣┫┃┃┣━━┃╰┫╭╮┃┃┃╰┫┃━┫╰╯┃╰╯┃┃┃╰┫┃━┫┃
+╰━━┻╯╰┻━━┻━┻╯╰┻╯╰━┻━━┫╭━┻━━┻╯╰━┻━━┻╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯"""
+
+urls = [
+    "GitHub - https://github.com/muneebwanee",
+    "Instagram - https://instagram.com/muneebwanee",
+    "Facebook - https://fb.com/muneebwane",
+    "Twitter - https://twitter.com/muneebwanee",
+    "InstaReporter Tool - https://github.com/muneebwanee/InstaReporter",
+    "Gmail - mailto:muneebwanee@gmail.com"
+    ]
+
+def print_logo():
+    print(Fore.RED + Style.BRIGHT + logo + Style.RESET_ALL + Style.BRIGHT +"\n")
+    print(Fore.MAGENTA + "      Producer: Muneeb"+ Style.RESET_ALL + Style.BRIGHT)
+    print(Fore.CYAN + "\n", "-> Follow me On Instagram @muneebwanee.")
+    print ("\n", "-> Special For Hackers:\n    " + choice(urls))
+    print(Style.RESET_ALL + Style.BRIGHT, Style.BRIGHT)
